ab31ba8b8b3e22ca3f2f590005bbf9a8c575c253
wenhong <wenhong.zw@antfin.com>
2018-11-09 17:32:08 +0800

    fix: className conflict to keyword like constuctor or toString

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ const processor = postcss.plugin('postcss-modules-scope', function(options) {
     const generateScopedName =
       (options && options.generateScopedName) || processor.generateScopedName;
 
-    const exports = {};
+    let exports = Object.create(null);
 
     function exportScopedName(name) {
       const scopedName = generateScopedName(
