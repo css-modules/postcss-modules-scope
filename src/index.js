fa0256ccc3a0e741b2ee74fe58d2754c63db54e8
Jason Quense <monastic.panic@gmail.com>
2019-02-15 13:42:01 -0500

    fix: don't rescope imported names

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-const postcss = require('postcss');
-const Tokenizer = require('css-selector-tokenizer');
+const postcss = require('postcss')
+const Tokenizer = require('css-selector-tokenizer')
 
-const hasOwnProperty = Object.prototype.hasOwnProperty;
+const hasOwnProperty = Object.prototype.hasOwnProperty
 
 function getSingleLocalNamesForComposes(selectors) {
   return selectors.nodes.map(node => {
@@ -12,9 +12,9 @@ function getSingleLocalNamesForComposes(selectors) {
         'composition is only allowed when selector is single :local class name not in "' +
           Tokenizer.stringify(selectors) +
           '"'
-      );
+      )
     }
-    node = node.nodes[0];
+    node = node.nodes[0]
     if (
       node.type !== 'nested-pseudo-class' ||
       node.name !== 'local' ||
@@ -26,9 +26,9 @@ function getSingleLocalNamesForComposes(selectors) {
           '", "' +
           Tokenizer.stringify(node) +
           '" is weird'
-      );
+      )
     }
-    node = node.nodes[0];
+    node = node.nodes[0]
     if (node.type !== 'selector' || node.nodes.length !== 1) {
       throw new Error(
         'composition is only allowed when selector is single :local class name not in "' +
@@ -36,9 +36,9 @@ function getSingleLocalNamesForComposes(selectors) {
           '", "' +
           Tokenizer.stringify(node) +
           '" is weird'
-      );
+      )
     }
-    node = node.nodes[0];
+    node = node.nodes[0]
     if (node.type !== 'class') {
       // 'id' is not possible, because you can't compose ids
       throw new Error(
@@ -47,42 +47,52 @@ function getSingleLocalNamesForComposes(selectors) {
           '", "' +
           Tokenizer.stringify(node) +
           '" is weird'
-      );
+      )
     }
-    return node.name;
-  });
+    return node.name
+  })
 }
+
 
 const processor = postcss.plugin('postcss-modules-scope', function(options) {
   return css => {
     const generateScopedName =
-      (options && options.generateScopedName) || processor.generateScopedName;
+      (options && options.generateScopedName) || processor.generateScopedName
 
-    const exports = Object.create(null);
+    const exports = Object.create(null)
+
+    // Find any :import and remember imported names
+    const importedNames = Object.create(null)
 
     function exportScopedName(name) {
+      // Don't change selectors that have already been replaced by a value import
+      // eg:
+      //   @value foo from './foo'
+      //   .bar .foo { color: red; }
+      if (importedNames[name]) return name;
+
       const scopedName = generateScopedName(
         name,
         css.source.input.from,
         css.source.input.css
-      );
-      exports[name] = exports[name] || [];
+      )
+      exports[name] = exports[name] || []
       if (exports[name].indexOf(scopedName) < 0) {
-        exports[name].push(scopedName);
+        exports[name].push(scopedName)
       }
-      return scopedName;
+      return scopedName
     }
 
     function localizeNode(node) {
-      const newNode = Object.create(node);
+      const newNode = Object.create(node)
       switch (node.type) {
         case 'selector':
-          newNode.nodes = node.nodes.map(localizeNode);
-          return newNode;
+          newNode.nodes = node.nodes.map(localizeNode)
+          return newNode
         case 'class':
         case 'id': {
-          newNode.name = exportScopedName(node.name);
-          return newNode;
+          newNode.name = exportScopedName(node.name)
+          return newNode
         }
       }
       throw new Error(
@@ -90,7 +100,7 @@ const processor = postcss.plugin('postcss-modules-scope', function(options) {
           ' ("' +
           Tokenizer.stringify(node) +
           '") is not allowed in a :local block'
-      );
+      )
     }
 
     function traverseNode(node) {
@@ -98,118 +108,117 @@ const processor = postcss.plugin('postcss-modules-scope', function(options) {
         case 'nested-pseudo-class':
           if (node.name === 'local') {
             if (node.nodes.length !== 1) {
-              throw new Error('Unexpected comma (",") in :local block');
+              throw new Error('Unexpected comma (",") in :local block')
             }
-            return localizeNode(node.nodes[0]);
+            return localizeNode(node.nodes[0])
           }
         /* falls through */
         case 'selectors':
         case 'selector': {
-          const newNode = Object.create(node);
-          newNode.nodes = node.nodes.map(traverseNode);
-          return newNode;
+          const newNode = Object.create(node)
+          newNode.nodes = node.nodes.map(traverseNode)
+          return newNode
         }
       }
-      return node;
+      return node
     }
 
-    // Find any :import and remember imported names
-    const importedNames = {};
+
     css.walkRules(rule => {
       if (/^:import\(.+\)$/.test(rule.selector)) {
         rule.walkDecls(decl => {
-          importedNames[decl.prop] = true;
-        });
+          importedNames[decl.prop] = true
+        })
       }
-    });
+    })
 
     // Find any :local classes
     css.walkRules(rule => {
-      const selector = Tokenizer.parse(rule.selector);
-      const newSelector = traverseNode(selector);
-      rule.selector = Tokenizer.stringify(newSelector);
+      const selector = Tokenizer.parse(rule.selector)
+      const newSelector = traverseNode(selector)
+      rule.selector = Tokenizer.stringify(newSelector)
       rule.walkDecls(/composes|compose-with/, decl => {
-        const localNames = getSingleLocalNamesForComposes(selector);
-        const classes = decl.value.split(/\s+/);
+        const localNames = getSingleLocalNamesForComposes(selector)
+        const classes = decl.value.split(/\s+/)
         classes.forEach(className => {
-          const global = /^global\(([^\)]+)\)$/.exec(className);
+          const global = /^global\(([^\)]+)\)$/.exec(className)
           if (global) {
             localNames.forEach(exportedName => {
-              exports[exportedName].push(global[1]);
-            });
+              exports[exportedName].push(global[1])
+            })
           } else if (hasOwnProperty.call(importedNames, className)) {
             localNames.forEach(exportedName => {
-              exports[exportedName].push(className);
-            });
+              exports[exportedName].push(className)
+            })
           } else if (hasOwnProperty.call(exports, className)) {
             localNames.forEach(exportedName => {
               exports[className].forEach(item => {
-                exports[exportedName].push(item);
-              });
-            });
+                exports[exportedName].push(item)
+              })
+            })
           } else {
             throw decl.error(
               `referenced class name "${className}" in ${decl.prop} not found`
-            );
+            )
           }
-        });
-        decl.remove();
-      });
+        })
+        decl.remove()
+      })
 
       rule.walkDecls(decl => {
-        var tokens = decl.value.split(/(,|'[^']*'|"[^"]*")/);
+        var tokens = decl.value.split(/(,|'[^']*'|"[^"]*")/)
         tokens = tokens.map((token, idx) => {
           if (idx === 0 || tokens[idx - 1] === ',') {
-            const localMatch = /^(\s*):local\s*\((.+?)\)/.exec(token);
+            const localMatch = /^(\s*):local\s*\((.+?)\)/.exec(token)
             if (localMatch) {
               return (
                 localMatch[1] +
                 exportScopedName(localMatch[2]) +
                 token.substr(localMatch[0].length)
-              );
+              )
             } else {
-              return token;
+              return token
             }
           } else {
-            return token;
+            return token
           }
-        });
-        decl.value = tokens.join('');
-      });
-    });
+        })
+        decl.value = tokens.join('')
+      })
+    })
 
     // Find any :local keyframes
     css.walkAtRules(atrule => {
       if (/keyframes$/i.test(atrule.name)) {
-        var localMatch = /^\s*:local\s*\((.+?)\)\s*$/.exec(atrule.params);
+        var localMatch = /^\s*:local\s*\((.+?)\)\s*$/.exec(atrule.params)
         if (localMatch) {
-          atrule.params = exportScopedName(localMatch[1]);
+          atrule.params = exportScopedName(localMatch[1])
         }
       }
-    });
+    })
 
     // If we found any :locals, insert an :export rule
-    const exportedNames = Object.keys(exports);
+    const exportedNames = Object.keys(exports)
     if (exportedNames.length > 0) {
-      const exportRule = postcss.rule({ selector: ':export' });
+      const exportRule = postcss.rule({ selector: ':export' })
       exportedNames.forEach(exportedName =>
         exportRule.append({
           prop: exportedName,
           value: exports[exportedName].join(' '),
-          raws: { before: '\n  ' }
+          raws: { before: '\n  ' },
         })
-      );
-      css.append(exportRule);
+      )
+      css.append(exportRule)
     }
-  };
-});
+  }
+})
 
 processor.generateScopedName = function(exportedName, path) {
   const sanitisedPath = path
     .replace(/\.[^\.\/\\]+$/, '')
     .replace(/[\W_]+/g, '_')
-    .replace(/^_|_$/g, '');
-  return `_${sanitisedPath}__${exportedName}`;
-};
+    .replace(/^_|_$/g, '')
+  return `_${sanitisedPath}__${exportedName}`
+}
 
-module.exports = processor;
+module.exports = processor
