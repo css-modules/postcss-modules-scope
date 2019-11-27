affc4a560575f6b0f88e4ec11713dde186adf8aa
evilebottnawi <sheo13666q@gmail.com>
2019-11-27 22:38:34 +0300

    fix: add additional space after the escape sequence

---
+++ test/test-cases/escape-sequence/options.js
@@ -0,0 +1,21 @@
+const cssesc = require('cssesc');
+
+const filenameReservedRegex = /[<>:"/\\|?*\x00-\x1F]/g;
+const reControlChars = /[\u0000-\u001f\u0080-\u009f]/g;
+const reRelativePath = /^\.+/;
+
+module.exports = {
+  generateScopedName: function(name) {
+    return cssesc(
+      name
+        .replace(/smile/, '😀')
+        .replace(/_with_A/g, 'A')
+        .replace(/^((-?[0-9])|--)/, '_$1')
+        .replace(filenameReservedRegex, '-')
+        .replace(reControlChars, '-')
+        .replace(reRelativePath, '-')
+        .replace(/\./g, '-'),
+      { isIdentifier: true }
+    );
+  },
+};
